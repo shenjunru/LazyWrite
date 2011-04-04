6877d4fa1ee335d445661361f6af8887cfe929b5
Shen Junru <shenjunru@gmail.com>
2011-04-04 18:06:35 +0800

    added demo files: index / basic-test.

---
+++ demo/basic-test-write.js
@@ -0,0 +1,2 @@
+console.log('execute external js');
+document.write(' external[js]<script type="text/javascript" src="basic-test-write.php" />');
---
+++ demo/basic-test-write.php
@@ -0,0 +1,2 @@
+<?php sleep(5); ?>
+document.write(' external[php]');
